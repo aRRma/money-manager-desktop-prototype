0a7d2f92bfdd32e0f24cc29ae39f502cc48b4cd1
Станислав Данильченко <arrma99@gmail.com>
2023-05-24 00:03:38 +0300

    запуск отчета после прогона тестов

--- MoneyManager.Core.Tests.NUnit/GlobalInitializationOfTests.cs
+++ MoneyManager.Core.Tests.NUnit/GlobalInitializationOfTests.cs
@@ -1,11 +1,15 @@
 ﻿using Allure.Net.Commons;
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace MoneyManager.Core.Tests.NUnit
 {
     [SetUpFixture]
     internal class GlobalInitializationOfTests
     {
+        private readonly string CMD_PATH = Path.Combine("C:\\Windows\\System32", "cmd.exe");
+        private readonly string ALLURE_RESULT_PATH = Path.Combine("./allure-results");
+
         /// <summary>
         /// Запускается один раз перед запуском всех тестов
         /// </summary>
@@ -21,7 +25,16 @@ namespace MoneyManager.Core.Tests.NUnit
         [OneTimeTearDown]
         public void TearDown()
         {
-            
+            // должен быть установлен allure и добавлен путь до него в окружение
+            var cmd = new Process();
+            cmd.StartInfo.FileName = CMD_PATH;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine($"allure serve {ALLURE_RESULT_PATH}");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
         }
     }
 }
