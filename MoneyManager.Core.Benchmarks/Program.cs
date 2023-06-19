446285b38bd629be93bc55e90708025d655b9bf2
Станислав Данильченко <arrma99@gmail.com>
2023-06-20 01:02:55 +0300

    добавил заготовку под бенчмарки

---
+++ MoneyManager.Core.Benchmarks/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Running;
+using Microsoft.EntityFrameworkCore;
+using MoneyManager.Core.DataBase;
+
+namespace MoneyManager.Core.Benchmarks
+{
+    // dotnet MoneyManager.Core.Benchmarks.dll -c Release
+
+    internal class Program
+    {
+        public static readonly AppDbContext DbContext;
+
+        private const string DB_NAME = "BenchDb.db";
+
+        static Program()
+        {
+            var optBuilder = new DbContextOptionsBuilder<AppDbContext>().UseSqlite($"Data Source = {DB_NAME}");
+            DbContext = new AppDbContext(optBuilder.Options, true);
+        }
+
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SomeTest>();
+        }
+    }
+}
---
+++ MoneyManager.Core.Benchmarks/SomeTest.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using MoneyManager.Core.DataBase.Models;
+
+namespace MoneyManager.Core.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SomeTest
+    {
+        private EfMoneyOperation entity;
+        private List<EfRecord> list;
+
+        [IterationSetup]
+        public void Setup()
+        {
+            entity = EfMoneyOperation.GetDefaultEntity();
+
+            list = new List<EfRecord>();
+            for (int i = 0; i < 100; i++)
+                list.Add(EfRecord.GetDefaultEntity());
+        }
+
+        [Benchmark, IterationCount(5)]
+        public void Test1()
+        {
+            Program.DbContext.MoneyOperations.Add(entity);
+        }
+
+        [Benchmark, IterationCount(5)]
+        public void Test2()
+        {
+            entity = EfMoneyOperation.GetDefaultEntity();
+            Program.DbContext.MoneyOperations.Add(entity);
+        }
+
+        [Benchmark, IterationCount(5)]
+        public void Test3()
+        {
+            Program.DbContext.Records.AddRange(list);
+        }
+
+        [Benchmark, IterationCount(5)]
+        public void Test4()
+        {
+            list = new List<EfRecord>();
+
+            for (int i = 0; i < 100; i++)
+                list.Add(EfRecord.GetDefaultEntity());
+
+            Program.DbContext.Records.AddRange(list);
+        }
+    }
+}
